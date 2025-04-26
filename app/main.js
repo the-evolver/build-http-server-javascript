6fdf85a4e2b130999316c18ab3647a8952a4a634
the-evolver <atulsharma98535@gmail.com>
2025-04-26 14:59:28 +0530

    sending response based on route

--- app/main.js
+++ app/main.js
@@ -7,8 +7,15 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
  // console.log(" socket ",socket);
   //socket.write(" Status : 200");
-  socket.on('data',()=> {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.on('data',(data)=> {
+    const dataStr = data.toString();
+    if(dataStr[5] == ' '){
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    }
+    else{
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+    
   })
   // socket.on("close", () => {
   //   socket.end();
