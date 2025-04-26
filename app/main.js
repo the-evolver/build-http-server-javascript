8cd3e4937c08abb27395be490b83c2656c06a93a
the-evolver <atulsharma98535@gmail.com>
2025-04-26 17:34:01 +0530

    adding /user-agent route

--- app/main.js
+++ app/main.js
@@ -8,19 +8,42 @@ const server = net.createServer((socket) => {
  
   socket.on('data',(data)=> {
     const dataStr = data.toString();
-    console.log(" data str => ",dataStr);
-    
-    let reqRoute = dataStr.split(' ')[1];
-    console.log('reqRoute',reqRoute,reqRoute.length);
+    //console.log(" data str => ",dataStr);
+  
+    let dataSplit  = dataStr.split(' ');
+    //console.log(' data split ',dataSplit);
+    let reqRoute = dataSplit[1];
+    //console.log('reqRoute',reqRoute,reqRoute.length);
     if(reqRoute.length == 1){
+      console.log(" In basic / get route ....");
       socket.write('HTTP/1.1 200 OK\r\n\r\n');
+      
     }else{
-      let echoRes = reqRoute.split('/')[2] || '';
-      if(echoRes){
-        socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${echoRes.length}\r\n\r\n${echoRes}`);
-      }else{
-        socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+      let parentRoute = reqRoute.split('/')[1]
+      if( parentRoute == 'echo'){
+          console.log(" In basic /echo get route ....");
+          let echoRes = reqRoute.split('/')[2] || '';
+          if(echoRes){
+            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${echoRes.length}\r\n\r\n${echoRes}`);
+          }
       }
+      else if(parentRoute == 'user-agent'){
+        console.log(" In basic /user-agent get route ....");
+        const dataReqArr = dataStr.split('\n');
+        let userAgentStr = '';
+        for(let str in dataReqArr){
+            if(str.startsWith("User-Agent")){
+                userAgentStr = str.split(':')[1].slice(1);
+                break;
+            }
+        }
+        console.log(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgentStr.length}\r\n\r\n${userAgentStr}`)
+        if(userAgentStr){
+          
+          socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgentStr.length}\r\n\r\n${userAgentStr}`)
+        }
+      }
+      socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
       
     }
     
