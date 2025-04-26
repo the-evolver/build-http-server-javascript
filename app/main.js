53c6853a47a70699a55d855f32e9adc306ed3f94
the-evolver <atulsharma98535@gmail.com>
2025-04-26 21:32:43 +0530

    adding file sending logic-3

--- app/main.js
+++ app/main.js
@@ -7,7 +7,7 @@ const net = require("net");
 const server = net.createServer((socket) => {
  
   socket.on('data',(data)=> {
-    console.log(" data ... ", data );
+    console.log(" data ... ", data.toString() );
     const dataStr = data.toString();
     let dataSplit  = dataStr.split(' ');
     let reqRoute = dataSplit[1];
