e08f60e91296dcb684e9d0b465e63aa7fcab4bd4
the-evolver <atulsharma98535@gmail.com>
2025-04-29 13:33:00 +0530

    header-compression-1

--- app/main.js
+++ app/main.js
@@ -62,6 +62,7 @@ const server = net.createServer((socket) => {
           
           if(echoRes){
             let encodingHeader = compress ? `Accept-Encoding: ${compressFormat}`:"" ;
+            console.log(" encoding header .... ",encodingHeader);
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${echoRes.length}\r\n${encodingHeader}\r\n${echoRes}`);
           }else{
             socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
